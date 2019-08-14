19074ffdbaaba1fd1fa06bdd992359fc954d0e95
Tai Dang <tai.dangit@gmail.com>
2019-08-14 01:17:31 -0700

    auth admin

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const postController = require("../controllers/postController");
 const validation = require("./validation");
+const helper = require("../auth/helpers");
 
 router.get("/topics/:topicId/posts/new", postController.new);
 router.get("/topics/:topicId/posts/:id", postController.show);
@@ -9,6 +10,7 @@ router.get("/topics/:topicId/posts/:id/edit", postController.edit);
 
 router.post(
   "/topics/:topicId/posts/create",
+  helper.ensureAuthenticated,
   validation.validatePosts,
   postController.create
 );
